6791f3e01880fc2c03307cd29327d225655a44ae
Bora Shin <tlsqhfk1103@gmail.com>
2022-05-25 15:10:01 +0900

    0525

--- js/main.js
+++ js/main.js
@@ -11,23 +11,28 @@
     
     
     /* ===================== MEET OUR HOTEL ===================== */
-    let hotelLogo = $('.hover_menu li a'),
+    let hotelLogo = $('.tab_menu li'),
         highlight = $('.highlight'),
-        hotelContent = $('#hotel_content div');
+        hotelContent = $('#hotel_content>div'),
+        contentLeft = $('#hotel_content>div:nth-child(-n+3)'),
+        hotelBg = $('.tab_menu li a')
       
         hotelLogo.click(function(e){
             e.preventDefault();
             let newLeft = $(this).position().left;
             let newWidth = $(this).outerWidth();
             let targetIdx = $(this).index();
+            
             activeTab(targetIdx);
 
             highlight.animate({left:newLeft,width:newWidth},300);
+            contentLeft.css({left:newLeft});
+            
         });
 
         function activeTab(num){
             hotelContent.hide();
-            hotelContent.eq(num).show();
+            hotelContent.eq(num).fadeIn();
         }
         activeTab(0);
     
