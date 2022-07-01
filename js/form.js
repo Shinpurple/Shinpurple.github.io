c10240413475801f9f6828dee6da976b47fd8440
Bora Shin <tlsqhfk1103@gmail.com>
2022-07-01 17:23:48 +0900

    070101

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
-$('#fullpage').fullpage({
-  autoScrolling:true,
-  scrollHorizontally: true
-});
+// $('#fullpage').fullpage({
+//   autoScrolling:true,
+//   scrollHorizontally: true
+// });
 
 
 /* ================== form ================== */
@@ -119,9 +119,3 @@ var dateFormat = "mm/dd/yy",
 
     //
 
-    $('input[name="dates"]').daterangepicker({
-      opens: 'left'
-    }, function(start, end, label) {
-      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    });
-  
