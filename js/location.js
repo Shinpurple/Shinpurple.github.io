1e0487f5ff3a47b53780bb29b24b2902de3f1895
Bora Shin <tlsqhfk1103@gmail.com>
2022-07-21 02:06:18 +0900

    07212121

--- js/location.js
+++ js/location.js
@@ -15,7 +15,7 @@ Menu.click(function(e){
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
         center: new kakao.maps.LatLng(37.5562855, 127.0052953), // 지도의 중심좌표
-        level: 6 // 지도의 확대 레벨
+        level: 8 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
